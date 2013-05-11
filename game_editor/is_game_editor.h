Plaristote/fallout-equestria

commit 30d2434b589938ba6df178b69744d57b034c40d5

+ Merged World object from the game engine and game editor. + Fixed object picking bug in game editor.

game_editor/mainwindow.cpp
@@ -345,22 +345,22 @@ void MainWindow::FilterInit()
 
 void MainWindow::CameraMoveBottom()
 {
-    my_task.camera->MoveV(-15.f);
+    my_task.camera->MoveV(-30.f);
 }
 
 void MainWindow::CameraMoveTop()
 {
-    my_task.camera->MoveV(15.f);
+    my_task.camera->MoveV(30.f);
 }
 
 void MainWindow::CameraMoveLeft()
 {
-    my_task.camera->MoveH(-15.f);
+    my_task.camera->MoveH(-30.f);
 }
 
 void MainWindow::CameraMoveRight()
 {
-    my_task.camera->MoveH(15.f);
+    my_task.camera->MoveH(30.f);
 }
 
 void MainWindow::DrawMap()
@@ -535,7 +535,7 @@ void MainWindow::PandaInitialized()
      waypointHovered  = 0;
 
     my_task.timer.start();
-    //AsyncTaskManager::get_global_ptr()->add(&my_task);
+    AsyncTaskManager::get_global_ptr()->add(&my_task);
 
     wizardDynObject = false;
     wizardMapObject = false;
@@ -621,7 +621,10 @@ void MainWindow::DynamicObjectSelect()
         ui->interObjScaleY->setValue(scale.get_y());
         ui->interObjScaleZ->setValue(scale.get_z());
         dialogObject.SetCurrentObject(dynamicObjectSelected);
+        ui->interObjEditor->setEnabled(true);
     }
+    else
+        ui->interObjEditor->setEnabled(false);
 }
 
 void MainWindow::DynamicObjectNameChanged(QString name)
@@ -1141,7 +1144,7 @@ void MainWindow::WaypointDiscardSelection(void)
 
 void MainWindow::WaypointSelectAll(void)
 {
-  std::list<Waypoint>::iterator it;
+  std::vector<Waypoint>::iterator it;
 
   for (it = world->waypoints.begin() ; it != world->waypoints.end() ; ++it)
   {
@@ -1195,14 +1198,15 @@ void MainWindow::WaypointSyncTerrain(void)
       segment->set_point_a(wp.get_x(), wp.get_y(), wp.get_z());
       segment->set_point_b(wp.get_x(), wp.get_y(), wp.get_z() - 100000.f);
 
+      cout << "Looking for collisions" << endl;
       col_traverser.add_collider(np, col_queue);
       col_traverser.traverse(world->window->get_render());
       if (col_queue->get_num_entries())
       {
         col_queue->sort_entries();
         min_pos = col_queue->get_entry(0)->get_surface_point(world->window->get_render());
-        /*std::cout << "Found a collision" << std::endl;
-        std::cout << min_pos.get_x() << ", " << min_pos.get_y() << ", " << min_pos.get_z() << std::endl;*/
+        std::cout << "Found a collision" << std::endl;
+        std::cout << min_pos.get_x() << ", " << min_pos.get_y() << ", " << min_pos.get_z() << std::endl;
 
         // ALMOST DONE !
         LPoint3 min_point, max_point;
@@ -1237,8 +1241,27 @@ void MainWindow::WaypointSyncTerrain(void)
   thread.start();
 }
 
+void MainWindow::MapFocused(void)
+{
+    LoadMap(ui->listMap->currentText());
+    disconnect(ui->tabWidget, SIGNAL(currentChanged(int)), this, SLOT(MapFocused()));
+}
+
 void MainWindow::LoadMap(const QString& path)
 {
+    if (ui->tabWidget->currentIndex() != 0)
+    {
+      connect(ui->tabWidget, SIGNAL(currentChanged(int)), this, SLOT(MapFocused()));
+      return ;
+    }
+    QPandaApplication::SetPandaEnabled(true);
+    my_task.camera->SetPosition(0, 0, 75);
+    mapobjectSelected     = 0;
+    mapobjectHovered      = 0;
+    dynamicObjectSelected = 0;
+    dynamicObjectHovered  = 0;
+    ui->objectEditor->setEnabled(false);
+    ui->interObjEditor->setEnabled(false);
     waypointsSelection.clear();
     levelName = path;
 

game_editor/world.h
@@ -22,6 +22,8 @@
 # include <algorithm>
 # include "serializer.hpp"
 
+# include "divide_and_conquer.hpp"
+
 # ifndef TRUE_SQRT
 #  define SQRT my_sqrt
 # else
@@ -82,27 +84,37 @@ struct Waypoint
     struct Arc
     {
         Arc(NodePath from, Waypoint* to);
+        Arc(const Arc&);
 	~Arc();
 
         bool operator==(Waypoint* other) { return (to == other); }
         void UpdateDirection(void);
         void Destroy(void);
+        
+        void SetVisible(bool);
 
         PT(CollisionSegment) csegment;
         PT(CollisionNode)    node;
         NodePath             nodePath;
+        NodePath             from;
         Waypoint*            to;
 
         ArcObserver*         observer;
     };
 
     typedef std::list<Arc> Arcs;
+    typedef std::vector<std::pair<Arc, unsigned short> > ArcsWithdrawed;
 
     unsigned int        id;
     unsigned char       floor;
     Arcs                arcs;
+    ArcsWithdrawed      arcs_withdrawed;
     NodePath            nodePath;
     FBoundingBox        mouseBox;
+    
+    void WithdrawArc(Waypoint* other);
+    void UnwithdrawArc(Waypoint* other, ArcObserver* observer);
+    std::pair<Arc, unsigned short>* GetWithdrawable(Waypoint* other);
 
     Waypoint(NodePath root);
     Waypoint(void) {}
@@ -110,6 +122,8 @@ struct Waypoint
     bool                 operator==(const Waypoint& other) const;
     bool                 operator==(const Waypoint* other) const;
     bool                 operator==(unsigned int id)       const { return (this->id == id); }
+    bool                 operator<(const Waypoint& other)  const { return (id < other.id); }
+    bool                 operator>(const Waypoint& other)  const { return (id > other.id); }
     Arcs::iterator       ConnectUnsafe(Waypoint* other);
     Arcs::iterator       Connect(Waypoint* other);
     Arcs::iterator       Disconnect(Waypoint* other);
@@ -119,14 +133,19 @@ struct Waypoint
     void                 UpdateArcDirection(Waypoint*);
     void                 SetSelected(bool);
     bool                 IsSelected(void) const { return (selected); }
+    
+    void                 SetArcsVisible(bool);
 
     // Pathfinding features
     float                GoalDistanceEstimate(const Waypoint& goal) const { return (GetDistanceEstimate(goal)); }
     float                GetDistanceEstimate(const Waypoint& other) const;
     std::list<Waypoint*> GetSuccessors(Waypoint* parent);
     float                GetCost(Waypoint&) { return (1.f); }
+    // Divide and conquer
+    LPoint3f             GetPosition(void) const { return (nodePath.get_pos()); }
 
     // Loading
+    void                 LoadArcs(void);
     void                 Unserialize(Utils::Packet& packet);
     void                 UnserializeLoadArcs(World*);
     void                 Serialize(Utils::Packet& packet);
@@ -181,7 +200,6 @@ struct DynamicObject : public MapObject
     Type      type;
 
     // All
-    std::string          name;
     std::string          script;
 
     // Door / Locker
@@ -206,6 +224,20 @@ struct DynamicObject : public MapObject
 struct Zone
 {
   bool operator==(const std::string& comp) { return (name == comp); }
+
+  bool Contains(unsigned int id) const
+  {
+    auto      it = waypoints.begin(), end = waypoints.end();
+
+    for (; it != end ; ++it)
+    {
+      if ((*it)->id == id)
+        return (true);
+    }
+    return (false);
+  }
+
+  bool Contains(Waypoint* wp) const { return (Contains(wp->id)); }
 
   std::string          name;
   std::list<Waypoint*> waypoints;
@@ -235,14 +267,13 @@ struct WorldLight
     Type_DynamicObject
   };
 
-  WorldLight(WindowFramework* window, Type type, ParentType ptype, NodePath parent, const std::string& name) : name(name), type(type), parent_type(ptype), parent(parent)
+  WorldLight(Type type, ParentType ptype, NodePath parent, const std::string& name) : name(name), type(type), parent_type(ptype), parent(parent), parent_i(0)
   {
-    enabled = true;
-    Initialize(window);
+    Initialize();
   }
-
-  WorldLight(NodePath parent) : parent(parent) {}
-
+  
+  WorldLight(NodePath parent) : parent(parent), parent_i(0) {}
+  
   void   SetEnabled(bool);
   void   Destroy(void);
 
@@ -260,9 +291,9 @@ struct WorldLight
 
   void   SetPosition(LPoint3 position)
   {
-     nodePath.set_pos(position);
+      nodePath.set_pos(position);
 #ifdef GAME_EDITOR
-     symbol.set_pos(position);
+      symbol.set_pos(position);
 #endif
   }
 
@@ -299,14 +330,14 @@ struct WorldLight
   NodePath    symbol;
 #endif
 private:
-  void Initialize(WindowFramework*);
+  void Initialize(void);
   NodePath    parent;
   MapObject*  parent_i;
 };
 
 struct World
 {
-    typedef std::list<Waypoint>      Waypoints;
+    typedef std::vector<Waypoint>    Waypoints;
     typedef std::list<MapObject>     MapObjects;
     typedef std::list<DynamicObject> DynamicObjects;
     typedef std::list<WorldLight>    WorldLights;
@@ -331,6 +362,8 @@ struct World
     WorldLights      lights;
 #ifdef GAME_EDITOR
     NodePath         lightSymbols;
+    bool             do_compile_doors;
+    bool             do_compile_waypoints;
 #endif
     
     ExitZones        exitZones;
@@ -345,7 +378,7 @@ struct World
     void      DeleteWayPoint(Waypoint*);
     Waypoint* GetWaypointFromNodePath(NodePath path);
     Waypoint* GetWaypointFromId(unsigned int id);
-    Waypoint* GetWaypointClosest(LPoint3);
+    Waypoint* GetWaypointClosest(LPoint3, unsigned char floor);
     void      SetWaypointsVisible(bool v)
     { if (v) { rootWaypoints.show();  } else { rootWaypoints.hide();  } }
     void           GetWaypointLimits(short currentFloor, LPoint3& upperRight, LPoint3& upperLeft, LPoint3& bottomLeft) const;
@@ -421,6 +454,7 @@ struct World
     void           AddExitZone(const std::string&);
     void           DeleteExitZone(const std::string&);
     ExitZone*      GetExitZoneByName(const std::string&);
+    bool           IsInExitZone(unsigned int id) const;
     
     void           AddEntryZone(const std::string&);
     void           DeleteEntryZone(const std::string&);
@@ -438,15 +472,18 @@ struct World
     typedef std::function<void (const std::string&, float)> ProgressCallback;
 
     void           UnSerialize(Utils::Packet& packet);
-    void           Serialize(Utils::Packet& packet, ProgressCallback progress_callback);
+#ifndef GAME_EDITOR
+    void           Serialize(Utils::Packet& packet);
+#else
+    void           Serialize(Utils::Packet& packet, std::function<void (const std::string&, float)> progress_callback);
+#endif
 
     void           CompileWaypoints(ProgressCallback);
     void           CompileDoors(ProgressCallback);
-
-#ifdef GAME_EDITOR
-    bool           do_compile_waypoints;
-    bool           do_compile_doors;
-#endif
+    
+    static NodePath model_sphere;
+    
+    DivideAndConquer::Graph<Waypoint, LPoint3f> waypoint_graph;
 };
 
 #endif // WORLD_H

include/level/world.h
@@ -274,6 +274,9 @@ struct WorldLight
   
   WorldLight(NodePath parent) : parent(parent), parent_i(0) {}
   
+  void   SetEnabled(bool);
+  void   Destroy(void);
+
   LColor GetColor(void) const
   {
     return (light->get_color());
@@ -284,6 +287,14 @@ struct WorldLight
     LColor color(r, g, b, a);
     
     light->set_color(color);
+  }
+
+  void   SetPosition(LPoint3 position)
+  {
+      nodePath.set_pos(position);
+#ifdef GAME_EDITOR
+      symbol.set_pos(position);
+#endif
   }
 
   bool operator==(const std::string& comp) { return (name == comp); }
@@ -312,8 +323,12 @@ struct WorldLight
   PT(Light)   light;
   Lens*       lens;
   float       zoneSize;
+  bool        enabled;
 
   std::list<NodePath> enlightened;
+#ifdef GAME_EDITOR
+  NodePath    symbol;
+#endif
 private:
   void Initialize(void);
   NodePath    parent;
@@ -336,7 +351,6 @@ struct World
 
     NodePath         rootWaypoints;
     Waypoints        waypoints;
-    NodePath         waypoint_maps;
 
     NodePath         rootMapObjects;
     MapObjects       objects;
@@ -346,6 +360,11 @@ struct World
     
     NodePath         rootLights;
     WorldLights      lights;
+#ifdef GAME_EDITOR
+    NodePath         lightSymbols;
+    bool             do_compile_doors;
+    bool             do_compile_waypoints;
+#endif
     
     ExitZones        exitZones;
     EntryZones       entryZones;
@@ -450,11 +469,17 @@ struct World
     
     Waypoint*      GetWaypointAt(LPoint2f);
 
-    void           UnSerialize(Utils::Packet& packet);
-    void           Serialize(Utils::Packet& packet);
+    typedef std::function<void (const std::string&, float)> ProgressCallback;
 
-    void           CompileWaypoints(void);
-    void           CompileDoors(void);
+    void           UnSerialize(Utils::Packet& packet);
+#ifndef GAME_EDITOR
+    void           Serialize(Utils::Packet& packet);
+#else
+    void           Serialize(Utils::Packet& packet, std::function<void (const std::string&, float)> progress_callback);
+#endif
+
+    void           CompileWaypoints(ProgressCallback);
+    void           CompileDoors(ProgressCallback);
     
     static NodePath model_sphere;
     

src/level/world.cpp
@@ -1,5 +1,4 @@
 #include "level/world.h"
-#include <timer.hpp>
 #include <panda3d/collisionHandlerQueue.h>
 
 using namespace std;
@@ -12,17 +11,19 @@ NodePath debug_pathfinding;
 
 World::World(WindowFramework* window)
 {
-  this->window       = window;
-  rootWaypoints      = window->get_render().attach_new_node("waypoints");
-  rootMapObjects     = window->get_render().attach_new_node("mapobjects");
-  rootDynamicObjects = window->get_render().attach_new_node("dynamicobjects");
-  rootLights         = window->get_render().attach_new_node("lights");
-  waypoint_maps      = window->get_render().attach_new_node("waypoint_maps");
-
-  model_sphere = window->load_model(window->get_panda_framework()->get_models(), "misc/sphere");
-  debug_pathfinding = window->get_render().attach_new_node("debug_pathfinding");
-  
-  rootWaypoints.set_collide_mask(ColMask::Waypoint);
+  this->window         = window;
+  model_sphere         = window->load_model(window->get_panda_framework()->get_models(), "misc/sphere");
+  rootWaypoints        = window->get_render().attach_new_node("waypoints");
+  rootMapObjects       = window->get_render().attach_new_node("mapobjects");
+  rootDynamicObjects   = window->get_render().attach_new_node("dynamicobjects");
+  rootLights           = window->get_render().attach_new_node("lights");
+#ifdef GAME_EDITOR
+  lightSymbols         = window->get_render().attach_new_node("lightSymbols");
+  do_compile_doors     = true;
+  do_compile_waypoints = true;
+#endif
+  debug_pathfinding    = window->get_render().attach_new_node("debug_pathfinding");
+  //rootWaypoints.set_collide_mask(ColMask::Waypoint);
 }
 
 World::~World()
@@ -371,6 +372,9 @@ EntryZone* World::GetEntryZoneByName(const std::string& name)
 void World::AddLight(WorldLight::Type type, const std::string& name)
 {
   lights.push_back(WorldLight(type, WorldLight::Type_None, rootLights, name));
+#ifdef GAME_EDITOR
+  lights.rbegin()->symbol.reparent_to(lightSymbols);
+#endif
 }
 
 void World::AddLight(WorldLight::Type type, const std::string& name, MapObject* parent)
@@ -469,12 +473,32 @@ void        World::CompileLight(WorldLight* light, unsigned char colmask)
   colNp.detach_node();
 }
 
+void WorldLight::SetEnabled(bool set_enabled)
+{
+  std::function<void (NodePath)> set_light;
+  std::function<void (NodePath)> unset_light;
+
+  enabled       = set_enabled;
+  set_light     = [this](NodePath object) { object.set_light(nodePath); };
+  unset_light   = [this](NodePath object) { object.clear_light(nodePath); };
+  for_each(enlightened.begin(), enlightened.end(), enabled ? set_light : unset_light);
+}
+
+void WorldLight::Destroy(void)
+{
+  SetEnabled(false);
+  nodePath.detach_node();
+#ifdef GAME_EDITOR
+  symbol.detach_node();
+#endif
+}
+
 // WAYPOINTS
 
 Waypoint::Waypoint(NodePath root)
 {
   nodePath = root;
-  nodePath.set_collide_mask(CollideMask(ColMask::Waypoint));
+  //nodePath.set_collide_mask(CollideMask(ColMask::Waypoint));
   nodePath.set_scale(2.f);
   nodePath.show();
 
@@ -1050,8 +1074,10 @@ void WorldLight::UnSerialize(World* world, Utils::Packet& packet)
   float     pos_x, pos_y, pos_z;
   float     hpr_x, hpr_y, hpr_z;
   string    parent_name;
+  char      tmp_enabled;
 
-  packet >> name >> zoneSize;
+  packet >> name >> tmp_enabled >> zoneSize;
+  enabled = tmp_enabled;
   packet.operator>> <char>(reinterpret_cast<char&>(type));
   packet.operator>> <char>(reinterpret_cast<char&>(parent_type));
   if (parent_type != Type_None)
@@ -1080,7 +1106,7 @@ void WorldLight::Serialize(Utils::Packet& packet)
 {
   LColor color = light->get_color();
 
-  packet << name << zoneSize << (char)type << (char)parent_type;
+  packet << name << (char)enabled << zoneSize << (char)type << (char)parent_type;
   if (parent_i)
     packet << parent_i->nodePath.get_name();
   packet << (float)color.get_x() << (float)color.get_y() << (float)color.get_z() << (float)color.get_w();
@@ -1116,20 +1142,6 @@ void           World::UnSerialize(Utils::Packet& packet)
       (*it).UnserializeLoadArcs(this);
       (*it).SetMouseBox();
     }
-
-    // Creating waypoint maps
-    /*{
-      bool moar_floors;
-      int  i = 0;
-
-      do
-      {
-        WaypointMap wp_map(window);
-
-        moar_floors = wp_map.Initialize(waypoints, i++);
-        wp_map.GetNodePath().reparent_to(waypoint_maps);
-      } while (moar_floors);
-    }*/
   }
 
   // MapObjects
@@ -1242,11 +1254,16 @@ void           World::UnSerialize(Utils::Packet& packet)
   for_each(lights.begin(), lights.end(), [this](WorldLight& light) { CompileLight(&light); });
 }
 
+#ifndef GAME_EDITOR
 void           World::Serialize(Utils::Packet& packet)
+#else
+void           World::Serialize(Utils::Packet& packet, std::function<void (const std::string&, float)> progress_callback)
+#endif
 {
   // Compile Step
 # ifdef GAME_EDITOR
-  CompileWaypoints();
+  if (do_compile_waypoints)
+    CompileWaypoints(progress_callback);
 # endif
 
   // Waypoints
@@ -1259,12 +1276,15 @@ void           World::Serialize(Utils::Packet& packet)
     while (it != end)
     {
       if ((*it).arcs.size() == 0)
-	it = waypoints.erase(it);
+        it = waypoints.erase(it);
       else
       {
-	(*it).id = ++id;
-	++it;
+        (*it).id = ++id;
+        ++it;
       }
+#ifdef GAME_EDITOR
+      progress_callback("Serializing Waypoints: ", (float)id / waypoints.size() * 100.f);
+#endif
     }
     size = waypoints.size();
     packet << size;
@@ -1288,7 +1308,8 @@ void           World::Serialize(Utils::Packet& packet)
     for (it = objects.begin() ; it != end ; ++it) { (*it).Serialize(packet); }
   }
 # ifdef GAME_EDITOR
-  CompileDoors();
+  if (do_compile_doors)
+    CompileDoors(progress_callback);
 # endif
 
   // DynamicObjects
@@ -1363,12 +1384,13 @@ void           World::Serialize(Utils::Packet& packet)
 }
 
 // MAP COMPILING
-void           World::CompileWaypoints(void)
+void           World::CompileWaypoints(ProgressCallback progress_callback)
 {
+    unsigned int        i   = 0;
     Waypoints::iterator it  = waypoints.begin();
     Waypoints::iterator end = waypoints.end();
 
-    for (; it != end ; ++it)
+    for (; it != end ; ++it, ++i)
     {
         Waypoint::Arcs::iterator itArc  = (*it).arcs.begin();
         Waypoint::Arcs::iterator endArc = (*it).arcs.end();
@@ -1410,15 +1432,17 @@ void           World::CompileWaypoints(void)
             //np.show();
             np.remove_node();
         }
+        progress_callback("Compiling Waypoints: ", (float)i / waypoints.size() * 100.f);
     }
 }
 
-void World::CompileDoors(void)
+void World::CompileDoors(ProgressCallback progress_callback)
 {
+  unsigned int        i   = 0;
   Waypoints::iterator it  = waypoints.begin();
   Waypoints::iterator end = waypoints.end();
 
-  for (; it != end ; ++it)
+  for (; it != end ; ++it, ++i)
   {
     Waypoint::Arcs::iterator itArc  = (*it).arcs.begin();
     Waypoint::Arcs::iterator endArc = (*it).arcs.end();
@@ -1460,6 +1484,7 @@ void World::CompileDoors(void)
       ++itArc;
       np.remove_node();
     }
+    progress_callback("Compiling Doors: ", (float)i / waypoints.size() * 100.f);
   }
 }
 

game_editor/FoE-Editor.pro
@@ -4,7 +4,7 @@
 #
 #-------------------------------------------------
 
-QT       += core gui
+QT       += core gui widgets
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 

commit 20e6bfd1f97c3f29a615e6924ddc5ae70ce88861

+ Fixed broken waypoint generation from 30d2434b5899

documents/milestones
@@ -99,7 +99,7 @@ x --> Various terrain and movement speed
 x --> Quantity Picker for inventories
 x --> Stairs
 x --> Shader for casting shadows
-  --> Advanced Light Management
+~ --> Advanced Light Management
 x --> Scatter inserted parties in the spawning zones
 x --> Per-context configuration for camera behaviors
 x --> Using skills to interact

game_editor/is_game_editor.h
@@ -1,0 +1,6 @@
+#ifndef  IS_GAME_EDITOR_H
+# define IS_GAME_EDITOR_H
+
+#define GAME_EDITOR
+
+#endif

game_editor/mainwindow.cpp
@@ -1144,7 +1144,7 @@ void MainWindow::WaypointDiscardSelection(void)
 
 void MainWindow::WaypointSelectAll(void)
 {
-  std::vector<Waypoint>::iterator it;
+  std::list<Waypoint>::iterator it;
 
   for (it = world->waypoints.begin() ; it != world->waypoints.end() ; ++it)
   {

game_editor/world.cpp
@@ -1,4 +1,9 @@
-#include "world.h"
+#include "is_game_editor.h"
+#ifdef GAME_EDITOR
+# include "world.h"
+#else
+# include "level/world.h"
+#endif
 #include <panda3d/collisionHandlerQueue.h>
 
 using namespace std;
@@ -140,6 +145,7 @@ Waypoint* World::GetWaypointFromNodePath(NodePath path)
   return (0);
 }
 
+#ifndef GAME_EDITOR
 Waypoint* World::GetWaypointFromId(unsigned int id)
 {
   if (waypoints.size() > id && id > 0)
@@ -153,6 +159,16 @@ Waypoint* World::GetWaypointFromId(unsigned int id)
   cout  << "Wp size -> " << waypoints.size() << ", looking for " << id << endl;
   return (0);
 }
+#else
+Waypoint* World::GetWaypointFromId(unsigned int id)
+{
+  Waypoints::iterator it = std::find(waypoints.begin(), waypoints.end(), id);
+
+  if (it != waypoints.end())
+    return (&(*it));
+  return (0);
+}
+#endif
 
 void World::FloorResize(int newSize)
 {

game_editor/world.h
@@ -30,7 +30,7 @@
 #  define SQRT sqrt
 # endif
 
-# define GAME_EDITOR
+# include "is_game_editor.h"
 
 # ifdef GAME_EDITOR
 #  define WAYPOINT_DEBUG
@@ -337,7 +337,11 @@ private:
 
 struct World
 {
+#ifndef GAME_EDITOR
     typedef std::vector<Waypoint>    Waypoints;
+#else
+    typedef std::list<Waypoint>      Waypoints;
+#endif
     typedef std::list<MapObject>     MapObjects;
     typedef std::list<DynamicObject> DynamicObjects;
     typedef std::list<WorldLight>    WorldLights;

include/is_game_editor.h
@@ -1,0 +1,6 @@
+#ifndef  IS_GAME_EDITOR_H
+# define IS_GAME_EDITOR_H
+
+//#define GAME_EDITOR
+
+#endif

include/level/world.h
@@ -30,7 +30,7 @@
 #  define SQRT sqrt
 # endif
 
-//# define GAME_EDITOR
+# include "is_game_editor.h"
 
 # ifdef GAME_EDITOR
 #  define WAYPOINT_DEBUG
@@ -337,7 +337,11 @@ private:
 
 struct World
 {
+#ifndef GAME_EDITOR
     typedef std::vector<Waypoint>    Waypoints;
+#else
+    typedef std::list<Waypoint>      Waypoints;
+#endif
     typedef std::list<MapObject>     MapObjects;
     typedef std::list<DynamicObject> DynamicObjects;
     typedef std::list<WorldLight>    WorldLights;

src/level/level.cpp
@@ -111,7 +111,6 @@ Level::Level(WindowFramework* window, GameUi& gameUi, Utils::Packet& packet, Tim
     std::cout << "Failed to load file" << std::endl;
   }
   
-  // TODO Implement map daylight/nodaylight system
   if (_world->sunlight_enabled)
     InitSun();
 

src/level/world.cpp
@@ -1,4 +1,9 @@
-#include "level/world.h"
+#include "is_game_editor.h"
+#ifdef GAME_EDITOR
+# include "world.h"
+#else
+# include "level/world.h"
+#endif
 #include <panda3d/collisionHandlerQueue.h>
 
 using namespace std;
@@ -140,6 +145,7 @@ Waypoint* World::GetWaypointFromNodePath(NodePath path)
   return (0);
 }
 
+#ifndef GAME_EDITOR
 Waypoint* World::GetWaypointFromId(unsigned int id)
 {
   if (waypoints.size() > id && id > 0)
@@ -153,6 +159,16 @@ Waypoint* World::GetWaypointFromId(unsigned int id)
   cout  << "Wp size -> " << waypoints.size() << ", looking for " << id << endl;
   return (0);
 }
+#else
+Waypoint* World::GetWaypointFromId(unsigned int id)
+{
+  Waypoints::iterator it = std::find(waypoints.begin(), waypoints.end(), id);
+
+  if (it != waypoints.end())
+    return (&(*it));
+  return (0);
+}
+#endif
 
 void World::FloorResize(int newSize)
 {