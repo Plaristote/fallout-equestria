Plaristote/fallout-equestria

commit 064ecc99badc124d155759bb9f21750bf637236e

+ MSVC compliance fixes

src/main.cpp
@@ -226,32 +226,44 @@ int main(int argc, char *argv[])
     WindowFramework* window;
     ConfigPage*      config = load_prc_file("config.prc");
 
+    cout << "[FoE] Loading configuration" << endl;
     if (!(PStatClient::connect("localhost", 5185))) // Initialize client for profiling collector
-      cout << "Can't connect to PStat client" << endl;
+      cout << "[FoE] Can't connect to PStat client" << endl;
+    cout << "[FoE] Opening Panda3D Framework" << endl;
     framework.open_framework(argc, argv);
+    cout << "[FoE] Setting window's title" << endl;
     framework.set_window_title("Fallout Equestria");
+    cout << "[FoE] Opening Window" << endl;
     window = framework.open_window();
-    window->enable_keyboard();
-    window->get_render().set_shader_auto();
+    cout << "[FoE] Enabling keyboard" << endl;
+    //window->enable_keyboard();
+    //window->get_render().set_shader_auto();
 
     // Set Windows Properties
+    cout << "[FoE] Setting window's properties" << endl;
     {
       WindowProperties props = window->get_graphics_window()->get_properties();
 
+      cout << "[FoE] Hiding system cursor" << endl;
       props.set_cursor_hidden(true);
+      cout << "[FoE] Setting window icon" << endl;
       props.set_icon_filename("data/icon.ico");
+      cout << "[FoE] Applying changes" << endl;
       window->get_graphics_window()->request_properties(props);
     }
 
+    cout << "[FoE] Loading configuration" << endl;
     OptionsManager::Initialize(); // Loads and handle conf.json file
     {
       MainMenu       mainMenu(window); // MainMenu will take over as a Panda3D async_task.
 
+      cout << "[FoE] Starting Main Loop" << std::endl;
       framework.main_loop();
       framework.close_framework();
 
       unload_prc_file(config);
     }
+    cout << "[FoE] Properly wrapping up." << endl;
     OptionsManager::Finalize();
     Script::Engine::Finalize();
   }

include/Boots/functorthread.hpp
@@ -1,68 +1,0 @@
-#ifndef  _SYNC_FUNCTOR_THREAD_HPP_
-# define _SYNC_FUNCTOR_THREAD_HPP_
-
-# include "thread.hpp"
-# include <functional>
-# include "observatory.hpp"
-
-namespace Sync
-{
-  template<typename RETURN> struct FunctorThreadFoo
-  {
-    typedef void (callback_fptr)(RETURN);
-  };
-
-  template<> struct FunctorThreadFoo<void>
-  {
-    typedef void (callback_fptr)();
-  };
-
-  template<typename RETURN>
-  class FunctorThread : public MyThread
-  {
-  public:
-    void         Set(std::function<RETURN (void)> functor)
-    {
-      this->functor = functor;
-    }
-
-  protected:
-    virtual void Run(void)
-    {
-      RunFunctor();
-      delete this;
-    }
-
-  public:
-    Sync::Signal<typename FunctorThreadFoo<RETURN>::callback_fptr> Done;
-
-    static FunctorThread* Create(std::function<RETURN (void)> functor)
-    {
-      FunctorThread* ptr = new FunctorThread;
-
-      ptr->Set(functor);
-      return (ptr);
-    }
-
-  private:
-    FunctorThread(void)
-    {
-    }
-
-    virtual ~FunctorThread(void)
-    {
-    }
-
-    std::function<RETURN (void)> functor;
-
-    void RunFunctor(void)
-    {
-      Done.Emit(functor());
-    }
-  };
-
-  template<>
-  void FunctorThread<void>::RunFunctor(void);
-}
-
-#endif

include/as_object.hpp
@@ -145,7 +145,7 @@ namespace AngelScript
         return (reinterpret_cast<TYPE>(context->GetReturnObject()));
       }
 
-      operator bool()   const { return (context->GetReturnByte());  }
+      operator bool()   const { return (context->GetReturnByte() != 0); }
       operator int()    const { return (context->GetReturnWord());  }
       operator long()   const { return (context->GetReturnDWord()); }
       operator float()  const { return (context->GetReturnFloat()); }

include/directory.hpp
@@ -1,6 +1,8 @@
 #ifndef  DIRECTORY_HPP
 # define DIRECTORY_HPP
 
+# include "globals.hpp"
+
 # ifndef _WIN32
 #  include <dirent.h>
 #  include <sys/stat.h>

include/gametask.hpp
@@ -1,6 +1,7 @@
 #ifndef  GAMETASK_HPP
 # define GAMETASK_HPP
 
+# include "globals.hpp"
 # include "level/level.hpp"
 # include "worldmap/worldmap.hpp"
 # include "playerparty.hpp"

include/gameui.hpp
@@ -1,6 +1,7 @@
 #ifndef  GAME_UI_HPP
 # define GAME_UI_HPP
 
+# include "globals.hpp"
 # include <panda3d/pgVirtualFrame.h>
 # include <panda3d/rocketRegion.h>
 # include <Rocket/Core.h>
@@ -194,7 +195,6 @@ private:
 };
 
 #include <queue>
-#include "Boots/functorthread.hpp"
 class LoadingScreen : public UiBase, public Sync::Semaphore
 {
   typedef std::queue<std::string> StringQueue;
@@ -207,7 +207,6 @@ public:
 private:
   void             Refresh(void);
 
-  Sync::FunctorThread<void>* thread;
   bool                       done;
 };
 

include/globals.hpp
@@ -12,6 +12,10 @@
 
 # define ABS(x) ((x) < 0 ? -(x) : (x))
 
+# ifdef _WIN32
+#  include <winsock2.h>
+# endif
+
 # include <panda3d/cmath.h>
 # include <algorithm>
 # include <string>

include/inventory_ui.hpp
@@ -1,8 +1,8 @@
 #ifndef  INVENTORY_UI_HPP
 # define INVENTORY_UI_HPP
 
-#include "level/inventory.hpp"
-
+# include "globals.hpp"
+# include "level/inventory.hpp"
 # include <Rocket/Core/Event.h>
 # include <Rocket/Core/EventListener.h>
 # include <Rocket/Core/Element.h>

commit 1a6b42ad99d985cb1206f20b0caeb06a06a096f6

+ Fixed character's fading regression from 064ecc9

src/main.cpp
@@ -237,7 +237,7 @@ int main(int argc, char *argv[])
     window = framework.open_window();
     cout << "[FoE] Enabling keyboard" << endl;
     //window->enable_keyboard();
-    //window->get_render().set_shader_auto();
+    window->get_render().set_shader_auto();
 
     // Set Windows Properties
     cout << "[FoE] Setting window's properties" << endl;