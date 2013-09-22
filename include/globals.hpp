Plaristote/fallout-equestria

commit c8e3a494dbddea432547038e8edce34710d9469c

+ Projectiles now scatter on the terrain when the character missed + Fixed crash caused by bad cleanup of InteractMenu + Fixed compatibility with Panda 1.7.9 (drag and drop was used in gameui.cpp)

include/globals.hpp
@@ -32,5 +32,8 @@ std::string underscore(const std::string&);
 #  define PT(PTTYPE) PointerTo< PTTYPE >
 # endif
 
+# if PANDA_MAJOR_VERSION > 1 || PANDA_MINOR_VERSION > 8
+#  define INVENTORY_USE_DRAGDROP
+# endif
 
 #endif // GLOBALS_HPP

src/inventory_ui.cpp
@@ -1,10 +1,6 @@
 #include "inventory_ui.hpp"
 #include <panda3d/pandaVersion.h>
 
-#if PANDA_MAJOR_VERSION > 1 || PANDA_MINOR_VERSION > 8
-# define INVENTORY_USE_DRAGDROP
-#endif
-
 /*
  * InventoryView
  */

include/level/character.hpp
@@ -119,13 +119,8 @@ public:
   const std::string   GetFactionName(void) const { return (_faction ? _faction->name : ""); }
   unsigned int        GetFaction(void) const     { return (_faction ? _faction->flag : 0);  }
 
-  unsigned short      GetActionPoints(void) const        { return (_actionPoints); }
-  void                SetActionPoints(unsigned short ap)
-  {
-    _actionPoints = ap;
-    if (_statistics)
-      ActionPointChanged.Emit(_actionPoints, Data(_statistics)["Statistics"]["Action Points"]);
-  }
+  unsigned short      GetActionPoints(void) const;
+  void                SetActionPoints(unsigned short ap);
   void                RestartActionPoints(void);
   
   short               GetHitPoints(void) const        { return (_hitPoints); }
@@ -144,10 +139,11 @@ public:
   void                DelFlag(unsigned char flag)       { if (HasFlag(flag)) { _flags -= flag; } }
   bool                HasFlag(unsigned char flag) const { return ((_flags & flag) != 0); }
 
-  void                PlayEquipedItemAnimation(unsigned short it, const std::string& name);
+  void                PlayEquipedItemAnimation(unsigned short it, const std::string& name, InstanceDynamicObject* target);
   void                RefreshEquipment(void);
   void                SetEquipedItem(unsigned short it, InventoryObject* object, EquipedMode mode = EquipedMouth);
   InventoryObject*    GetEquipedItem(unsigned short it);
+  NodePath            GetEquipedItemNode(unsigned short it);
   unsigned char       GetequipedAction(unsigned short it) const { return (_equiped[it].actionIt); }
   void                UnequipItem(unsigned short it);
   void                ItemNextUseType(unsigned short it);

include/level/divide_and_conquer.hpp
@@ -36,7 +36,6 @@ namespace DivideAndConquer
       
       virtual const std::vector<T*>& GetEntries(POS position_from, Heuristic heuristic)
       {
-        std::cout << "GetEntries not final node" << std::endl;
         return (FindBestNode(position_from, heuristic)->GetEntries(position_from, heuristic));
       }      
 

include/level/inventory.hpp
@@ -36,9 +36,9 @@ public:
   ~InventoryObject();
 
   int               HitSuccessRate(ObjectCharacter* user, ObjectCharacter* target, unsigned char useType);
-  const std::string UseAsWeapon(ObjectCharacter* user, ObjectCharacter* target, unsigned char useType);
-  const std::string UseOn(ObjectCharacter* user, InstanceDynamicObject* target, unsigned char useType);
-  const std::string Use(ObjectCharacter* user, unsigned char useType);
+  bool              UseAsWeapon(ObjectCharacter* user, ObjectCharacter* target, unsigned char useType);
+  bool              UseOn(ObjectCharacter* user, InstanceDynamicObject* target, unsigned char useType);
+  bool              Use(ObjectCharacter* user, unsigned char useType);
   const std::string GetName(void) const { return (this->Key());             }
   const std::string GetIcon(void) const { return ((*this)["icon"].Value()); }
   DynamicObject*    CreateDynamicObject(World* world) const;
@@ -54,7 +54,7 @@ public:
 
 private:
   template<class C>
-  const std::string ExecuteHook(const std::string& hook, ObjectCharacter* user, C* target, unsigned char actionIt);
+  bool              ExecuteHook(const std::string& hook, ObjectCharacter* user, C* target, unsigned char actionIt);
   
   DataTree           _dataTree;
   

include/level/projectile.hpp
@@ -8,6 +8,7 @@
 # include <panda3d/pointParticleRenderer.h>
 # include <panda3d/particleSystem.h>
 # include "level/world.h"
+# include "datatree.hpp"
 
 class ObjectCharacter;
 class InventoryObject;
@@ -15,21 +16,24 @@ class InventoryObject;
 class Projectile
 {
 public:
-  Projectile(World* world, NodePath parent);
+  Projectile(World* world, NodePath parent, NodePath target, Data data);
   ~Projectile(void);
 
   static Projectile* Factory(World*, ObjectCharacter*, InventoryObject*);
 
   NodePath GetNodePath(void) const { return (node_path); }
 
-  bool     HasExpired(void) const { return (time_left <= 0); }
+  bool     HasExpired(void) const;
+  bool     HasReachedDestination(void) const;
   void     SetTimeout(float timeout);
   void     SetColor(float red, float green, float blue, float alpha);
   void     SetAttenuation(float a, float b, float c);
   void     Run(float elapsed_time);
 
 private:
-  NodePath           node_path, light_node, enlightened;
+  NodePath           node_path, light_node, enlightened, target;
+  bool               is_moving;
+  float              speed;
   PT(ParticleSystem) system;
   PT(PointLight)     light;
   float              timeout;

include/level/world.h
@@ -104,6 +104,7 @@ struct Waypoint
     void WithdrawArc(Waypoint* other);
     void UnwithdrawArc(Waypoint* other, ArcObserver* observer);
     std::pair<Arc, unsigned short>* GetWithdrawable(Waypoint* other);
+    Waypoint*                       GetRandomWaypoint(void) const;
 
     Waypoint(NodePath root);
     Waypoint(void) {}

src/gameui.cpp
@@ -865,8 +865,10 @@ void GameInventory::SetEquipedItem(unsigned short slot, InventoryObject* item)
         int item_count = _inventory->GetObjectIterator(item);
 
         rml << "<p id='" << item_count << "' class='equiped_image";
+#ifdef INVENTORY_USE_DRAGDROP
         if (!(item->IsHidden()))
           rml << " inventory-item-draggable";
+#endif
         if ((*item)["icon"].Value() != "")
           rml << "'><img src='../textures/itemIcons/" << (*item)["icon"].Value() << "' />";
         rml << "</p>";

commit 3b70814ebfa22ee4c6a2fa379f1cadae1e641321

+ Fixed regression from c8e3a494dbddea432547038e8edce34710d9469c

include/globals.hpp
@@ -32,7 +32,9 @@ std::string underscore(const std::string&);
 #  define PT(PTTYPE) PointerTo< PTTYPE >
 # endif
 
-# if PANDA_MAJOR_VERSION > 1 || PANDA_MINOR_VERSION > 8
+# include <panda3d/pandaVersion.h>
+
+# if PANDA_MAJOR_VERSION >= 1 || PANDA_MINOR_VERSION >= 8
 #  define INVENTORY_USE_DRAGDROP
 # endif
 

src/inventory_ui.cpp
@@ -1,5 +1,4 @@
 #include "inventory_ui.hpp"
-#include <panda3d/pandaVersion.h>
 
 /*
  * InventoryView
@@ -95,6 +94,7 @@ void InventoryView::UpdateView(void)
 	  stream << "<span class='inventory-item-quantity'>x" << quantity << "</span>";
 	stream << "</span>";
         rml += stream.str();
+        cout << stream.str() << endl;
       }
     }
   }